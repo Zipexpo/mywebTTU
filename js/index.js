7bf15834d847aadfaa5905c48b3f6ea233146170
nvtNgan <nvtngan@hcmus.edu.vn>
2018-09-03 14:46:26 -0700

    hehe

--- js/index.js
+++ js/index.js
@@ -8,42 +8,31 @@ window.onload=function(){
 || document.documentElement.clientHeight
 || document.body.clientHeight) - margin.top - margin.bottom;
 	//scale
-	var csize = d3.scaleLinear().range([0,5]);
+	var csize = d3.scaleLinear().range([0,Math.min(width,height)/150]);
 	var color = d3.scaleOrdinal(d3.schemeCategory10);
 	//sceen
 	var svg = d3.select("svg");
-	var node,
-		net,
-		expand = {};
+    var range = Math.min(width,height)-20;
 	//other
-	var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    .force("link", d3.forceLink().distance(function(l, i) {
-		      var n1 = l.source, n2 = l.target;
-		    return 30 +
-		      Math.min(20 * Math.min((n1.size || (n1.group != n2.group ? n1.group_data.size : 0)),
-		                             (n2.size || (n1.group != n2.group ? n2.group_data.size : 0))),
-		           -30 +
-		           30 * Math.min((n1.link_count || (n1.group != n2.group ? n1.group_data.link_count : 0)),
-		                         (n2.link_count || (n1.group != n2.group ? n2.group_data.link_count : 0))),
-		           100);
-		      //return 150;
-		    }))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter((width)/2, height/2-100))
+    var simulation = d3.forceSimulation()
+        .force("link", d3.forceLink().id(function(d) { return d.index }))
+        .force("collide",d3.forceCollide( function(d){return csize(d.size) + 10 }).iterations(16) )
+        .force("charge", d3.forceManyBody())
+        .force("center", d3.forceCenter((width)/2, height/2-100))
+        .force("y", d3.forceY(0))
+        .force("x", d3.forceX(0));
 	//data
 	d3.json("data/index.json").then(function(data) {
-		net = network(data, net, getGroup, expand);
 		var link = svg.append("g")
       	.attr("class", "links")
 	    .selectAll("line")
-	    .data(net.links)
+	    .data(data.links)
 	    .enter().append("line")
 	      .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
 
 		node = svg.append("g")
 		.selectAll(".sum")
-		.data(net.nodes,function(d) { return d.id; })
+		.data(data.nodes,function(d) { return d.id; })
 		.enter().append("circle")
 			.attr("class", "sum")
 			.attr("r", function(d){ return csize(d.size);})
@@ -51,13 +40,24 @@ window.onload=function(){
 	          .on("start", dragstarted)
 	          .on("drag", dragged)
 	          .on("end", dragended));
-		simulation.force("charge", d3.forceManyBody().strength(function(){return -Math.max(width,height)/net.nodes.length*20}));
+		svg.selectAll(".sum")
+			.filter(function(d,i){if(d.image!=null) console.log(d.image); return d.image;})
+			.append("svg:image")
+				.attr("class","resresponsive-img circle")
+				.attr("src",d.img);
+        svg.selectAll(".sum")
+            .filter(function(d,i){if(d.icon!=null) console.log(d.icon); return d.icon;})
+            .append("i")
+            .attr("class",function(d){
+                return "icon fa fa-5x fa-" + d.icon;
+            })
+            .style(defaultStyle);
 		simulation
-		  .nodes(net.nodes)
+		  .nodes(data.nodes)
 		  .on("tick", ticked);
 
-		simulation.force("link")
-		  .links(net.links);
+        simulation.force("link")
+            .links(data.links);
 
 		
 
@@ -89,82 +89,5 @@ window.onload=function(){
 	  if (!d3.event.active) simulation.alphaTarget(0);
 	  d.fx = null;
 	  d.fy = null;
-	}
-
-	function getGroup(n) { return n.group; }
-
-	function network(data, prev, index, expand) {
-	  expand = expand || {};
-	  var gm = {},    // group map
-	      nm = {},    // node map
-	      lm = {},    // link map
-	      gn = {},    // previous group nodes
-	      gc = {},    // previous group centroids
-	      nodes = [], // output nodes
-	      links = []; // output links
-	  // process previous nodes for reuse or centroid calculation
-	  if (prev) {
-	    prev.nodes.forEach(function(n) {
-	      var i = index(n), o;
-	      if (n.size > 0) {
-	        gn[i] = n;
-	        n.size = 0;
-	      } else {
-	        o = gc[i] || (gc[i] = {x:0,y:0,count:0});
-	        o.x += n.x;
-	        o.y += n.y;
-	        o.count += 1;
-	      }
-	    });
-	  }
-	  // determine nodes
-	  for (var k=0; k<data.nodes.length; ++k) {
-	    var n = data.nodes[k],
-	        i = index(n),
-	        l = gm[i] || (gm[i]=gn[i]) || (gm[i]={group:i, size:0, nodes:[]});
-	    if (expand[i]) {
-	      // the node should be directly visible
-	      nm[n.name] = nodes.length;
-	      nodes.push(n);
-	      if (gn[i]) {
-	        // place new nodes at cluster location (plus jitter)
-	        n.x = gn[i].x + Math.random();
-	        n.y = gn[i].y + Math.random();
-	      }
-	    } else {
-	      // the node is part of a collapsed cluster
-	      if (l.size == 0) {
-	        // if new cluster, add to set and position at centroid of leaf nodes
-	        nm[i] = nodes.length;
-	        nodes.push(l);
-	        if (gc[i]) {
-	          l.x = gc[i].x / gc[i].count;
-	          l.y = gc[i].y / gc[i].count;
-	        }
-	      }
-	      l.nodes.push(n);
-	    }
-	  // always count group size as we also use it to tweak the force graph strengths/distances
-	    l.size += 1;
-	  n.group_data = l;
-	  }
-	  for (i in gm) { gm[i].link_count = 0; }
-	  // determine links
-	  for (k=0; k<data.links.length; ++k) {
-	    var e = data.links[k],
-	        u = index(e.source),
-	        v = index(e.target);
-	  if (u != v) {
-	    gm[u].link_count++;
-	    gm[v].link_count++;
-	  }
-	    u = expand[u] ? nm[e.source.name] : nm[u];
-	    v = expand[v] ? nm[e.target.name] : nm[v];
-	    var i = (u<v ? u+"|"+v : v+"|"+u),
-	        l = lm[i] || (lm[i] = {source:u, target:v, size:0});
-	    l.size += 1;
-	  }
-	  for (i in lm) { links.push(lm[i]); }
-	  return {nodes: nodes, links: links};
 	}
 }
